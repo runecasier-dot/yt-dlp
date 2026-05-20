aed3c34982398813060bc01a4105865450354493
runecasier-dot <rune.casier@gmail.com>
2026-05-20 11:22:14 +0200

    Update yt-dlp.cmd

--- yt-dlp.cmd
+++ yt-dlp.cmd
@@ -1,1 +1,6 @@
-@py -Werror -Xdev "%~dp0yt_dlp\__main__.py" %*
+curl ^"https://vod.vrtcdn.be/remix/content/vod/pl-d9c2d7d3-bf7b-4098-bf81-880cf60929e7/pl-d9c2d7d3-bf7b-4098-bf81-880cf60929e7_drm_254685ab-9f06-4a9f-9a55-5a332171c8dc.ism/.mpd?filter=^%^28^%^21^%^28type^%^3D^%^3D^%^22audio^%^22^%^26^%^26FourCC^%^21^%^3D^%^22AACL^%^22^%^29^%^29^" ^
+  -H ^"sec-ch-ua-platform: ^\^"Windows^\^"^" ^
+  -H ^"Referer: https://www.vrt.be/^" ^
+  -H ^"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36^" ^
+  -H ^"sec-ch-ua: ^\^"Chromium^\^";v=^\^"148^\^", ^\^"Google Chrome^\^";v=^\^"148^\^", ^\^"Not/A)Brand^\^";v=^\^"99^\^"^" ^
+  -H ^"sec-ch-ua-mobile: ?0^"
